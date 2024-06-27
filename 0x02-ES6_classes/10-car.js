f1fc13c10f3f3d25ddadf67bf628a5d45c8eecd9
Anphimens <philipinamensah19@gmail.com>
2024-06-27 16:41:03 +0100

    Vroom vroom, car branding

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const ClonedCar = this.constructor[Symbol.species] || this.constructor;
+    return new ClonedCar();
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+}
